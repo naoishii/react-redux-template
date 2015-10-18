a0cf6fc38f9fc9e817343542196bbfe8754f0a22
naoishii <drapuni@gmail.com>
2015-10-18 19:13:48 +0900

    とちゅう

--- gulp/tasks/webpack.babel.js
+++ gulp/tasks/webpack.babel.js
@@ -1,9 +1,9 @@
-var gulp = require('gulp');
-var webpack = require('gulp-webpack');
-var config = require('../config');
+import gulp from 'gulp';
+import webpack from 'gulp-webpack';
+import config from '../config';
 
 // タスク名はファイル名と同じにしておくと見通しが良い
-gulp.task('webpack', function () {
+gulp.task('webpack', () => {
     gulp.src(config.webpack.entry)
         .pipe(webpack(config.webpack))
         .pipe(gulp.dest(config.js.dest));
